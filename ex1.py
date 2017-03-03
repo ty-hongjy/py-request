9cc0898a196f8e5de965647b9af64c0eee2d55a3
ty-hongjy <hongjy1@163.com>
2017-03-03 16:43:34 +0800

    0303

--- ex1.py
+++ ex1.py
@@ -1,21 +1,22 @@
 import requests
+import os
 
 def getHTMLText(url):
 	try:
 		r=requests.get(url,timeout=30)
-		r.raise_for_status
+		r.raise_for_status()
 		r.status_code
-		r.encoding
+		print(r.encoding)
 		print(r.apparent_encoding)
-		q.head(url)
+		r.encoding=r.apparent_encoding
+		print(r.text[:1000])
+		#q.head(url)
 		#print(r.head(url))
 		r.text
 		#q=requests.head(url)
-		#print(q.headers)
 
 	except:
 		#return "err"
-		#//raise
 		#print("err")
 		return "err"
  
@@ -37,3 +38,42 @@ print(r.url)
 hd={'user-agent':'chrome/10'}
 r=requests.request('POST','http://pt123.io/wa',headers=hd)
 print(r.url)
+
+#####jd
+print("JD search")
+url="https://item.jd.com/3857525.html?cpdad=1DLSUE"
+getHTMLText(url)
+
+#baidu search
+print("Baidu search")
+kv={'wd':'Python'}
+r=requests.get("http://www.baidu.com/s",params=kv)
+print(r.status_code)
+print(len(r.text))
+
+##get pictures
+print("get pictures")
+
+url="http://image.nationalgeagraphics.com.cn/2017/0211/20170211061910157.jpg"
+root="D://pics//"
+path=root+url.split('/')[-1]
+try:
+	if  not os.path.exists(root):
+		os.mkdir(root)
+	if not os.path.exists(path):
+		r=requests.get(url)
+		with open(path,'wb') as f:
+			f.write(r.content)
+			f.close()
+			print("success")
+	else:
+		print("file exists")
+except:
+	print("get failse")
+
+##search IP
+print("search IP")
+url="http://m.ip138.com/ip.asp?ip="
+url=url+'202.204.80.112'
+getHTMLText(url)
+
