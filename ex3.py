1d0bc95cec7bdd8803ce189c17eb26689b4e57a7
ty-hongjy <hongjy1@163.com>
2017-03-13 11:28:58 +0800

    0310

--- ex3.py
+++ ex3.py
@@ -5,6 +5,11 @@ import bs4
 def getHTMLText(url):
 	try:
 		r=requests.get(url,timeout=30)
+		print(r.status_code)
+		print(r.encoding)
+		print(r.apparent_encoding)
+		r.encoding=r.apparent_encoding
+
 		r.raise_for_status()
 		print('ok')
 		r.encoding=r.apparent_encoding
@@ -31,6 +36,9 @@ def printUnivList(ulist,num):
 def main():
 	 uinfo=[]
 	 url='http://www.zuihaodaxue.cn/zuihaodaxuepaiming2016.html'
+	 url='http://www.zuihaodaxue.cn/news/20170227-332.html'
+	 url='http://learning.sohu.com/20170227/n481870520.shtml'
+
 	 html=getHTMLText(url)
 	 print(html)
 	 fillUnivList(uinfo,html)
