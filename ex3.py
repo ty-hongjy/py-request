ef9e5a5093058d19dede0d27b9789c154c1a50d8
ty-hongjy <hongjy1@163.com>
2017-03-13 12:38:19 +0800

    0313

--- ex3.py
+++ ex3.py
@@ -6,17 +6,14 @@ def getHTMLText(url):
 	try:
 		r=requests.get(url,timeout=30)
 		print(r.status_code)
-		print(r.encoding)
-		print(r.apparent_encoding)
+		#print(r.encoding)
+		#print(r.apparent_encoding)
 		r.encoding=r.apparent_encoding
 
 		r.raise_for_status()
-		print('ok')
-		r.encoding=r.apparent_encoding
 		return r.text
 	except:
-		return ""
-
+		return "err"
 
 
 def fillUnivList(ulist, html):
@@ -28,18 +25,21 @@ def fillUnivList(ulist, html):
 
 
 def printUnivList(ulist,num):
-	 print("{:^10}\t{:^6}\t{:^10}".format("排名","学校名称","总分"))
+	 print("{:^10}\t{:^20}\t{:^10}".format("排名","学校名称","总分"))
 	 for i in range(num):
 		 u=ulist[i]
-		 print("{:^10}\t{:^6}\t{:^10}".format(u[0],u[1],u[2]))
+		 print("{:^10}\t{:^20}\t{:^10}".format(u[0],u[1],u[2]))
 
 def main():
 	 uinfo=[]
 	 url='http://www.zuihaodaxue.cn/zuihaodaxuepaiming2016.html'
-	 url='http://www.zuihaodaxue.cn/news/20170227-332.html'
-	 url='http://learning.sohu.com/20170227/n481870520.shtml'
+	 #url='http://www.zuihaodaxue.cn/news/20170227-332.html'
+	 #url='http://learning.sohu.com/20170227/n481870520.shtml'
 
 	 html=getHTMLText(url)
-	 print(html)
+	 #print(html)
 	 fillUnivList(uinfo,html)
 	 printUnivList(uinfo,20) #20 univs main()
+
+if __name__ == '__main__':
+	main()
