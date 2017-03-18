0c6f929a462ac8ea069b4fc4c763e5b3d284a9c2
ty-hongjy <hongjy1@163.com>
2017-03-18 13:51:16 +0800

    0318

--- CrawMoocList.py
+++ CrawMoocList.py
@@ -9,49 +9,63 @@ Created on Fri Mar 17 14:46:49 2017
 
 import requests
 import re 
+from bs4 import BeautifulSoup
 
 def getHTMLText(url):
     try:
-        r=requests.get(url,timeout=30)
+        kv={'user-agent':'Mozilla/5.0'}
+        r=requests.get(url,headers=kv,timeout=30)
+        #r=requests.get(url,timeout=30)
         r.raise_for_status()
         r.encoding=r.apparent_encoding
         return r.text
     except:
-        return ""
+        return "err"
 
-def parsePage(ilt,html):
+def CategoryList(ilt,html):
+    #print(html)
+    #html=getHTMLText(course_url)
+    #soup=BeautifulSoup(html,'html.parser')
+    #soupbody=soup.body.contents
+    #print(soup.body.prettify())
+    #soup.get(attrs)
+    #ls=re.findall('r\"data-cate=*\"',html)
+    #print(ls)
+
     try:
-        plt=re.findall(r'\"view_price\"\:\"[\d\.]*\"',html)
-        tlt=re.findall(r'\"raw_title\"\:\".*?\"',html)
-        for i in range(len(plt)):
-            price=eval(plt[i].split(':')[1])
-            title=eval(tlt[i].split(':')[1])
-            ilt.append([price,title])
+        tlt=re.findall(r'data-label="..{2,5}"',html)
+        print(tlt)
+        print(len(tlt))
+        
+        for i in range(len(tlt)):
+                cate=eval(tlt[i].split('=')[1])
+                print(cate)
+                cate1=re.findall(r'.*[^\d{1,2}]',cate)
+                print(cate1)
+                ilt.append(cate1)
     except:
         print("")
 
-def printGoodsList(ilt):
-    tplt="{:4}\t{:8}\t{:16}"
-    print(tplt.format("序号","价格","商品名称"))
+def printCategoryList(ilt):
+    tplt="{:4}\t{:8}"
+    print(tplt.format("序号","分类"))
     count=0
     for g in ilt:
         count=count+1
-        print(tplt.format(count,g[0],g[1]))
+        print(tplt.format(count,g[0]))
+
 
 def main():
-    goods='书包'
-    depth=3
-    start_url='http://www.icourse163.org/category/computer'
     infoList=[]
-    for i in range(depth):
-        try:
-            url=start_url+'&s='+str(44*i)
-            html=getHTMLText(url)
-            parsePage(infoList,html)
-        except:
-            continue
-    printGoodsList(infoList)
-    
 
-main()
+    category_url='http://www.icourse163.org/category/all'
+    category_url='http://www.icourse163.org/'
+
+    course_url='http://www.icourse163.org/category/computer'
+    html=getHTMLText(category_url)
+    CategoryList(infoList,html)
+    printCategoryList(infoList)
+    
+if __name__ == '__main__':
+    main()
         
